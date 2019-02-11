f13b08147c8fc79aba003fc6b813a63940691c4e
VictorWangwz <wangzhen5161@outlook.com>
2019-02-10 19:44:11 -0800

    cannot fetch keylistener from the registered list

--- jpacman-framework/src/test/java/org/jpacman/test/framework/ui/PacmanKeyListenerTest.java
+++ jpacman-framework/src/test/java/org/jpacman/test/framework/ui/PacmanKeyListenerTest.java
@@ -1,31 +1,34 @@
 package org.jpacman.test.framework.ui;
 
+import java.awt.AWTException;
+import java.awt.Robot;
 import java.awt.event.KeyEvent;
 
 import org.jpacman.framework.factory.FactoryException;
 import org.jpacman.framework.ui.IPacmanInteraction;
 import org.jpacman.framework.ui.MainUI;
 import org.jpacman.framework.ui.PacmanKeyListener;
+import org.junit.Before;
 import org.junit.Test;
 
 public class PacmanKeyListenerTest {
-	/**
-	 * Invoke series of actions on the real game, including the 
-	 * real ghost mover.
-	 * 
-	 * @throws FactoryException If reading board fails.
-	 * @throws InterruptedException Since we're timing.
-	 */
+	KeyEvent event;
+	MainUI ui = new MainUI();
+	@Before
+	public void setUp(){
+		this.event = new KeyEvent(this.ui, 0, 0, 0, KeyEvent.VK_UP, 'a');
+		}
 	@Test
-	public void testUIActions() throws FactoryException, InterruptedException {
-		MainUI ui = new MainUI();
+	public void testUIActions() throws FactoryException, InterruptedException, AWTException {
+
+		Robot robot = new Robot();
 		ui.initialize();
 		ui.start();
 		IPacmanInteraction eventHandler = ui.eventHandler();
-		PacmanKeyListener[] keyListener = ui.getListeners(PacmanKeyListener.class);
-		System.out.print(keyListener);
-
-		// now trigger interesting events.
-		eventHandler.start();
+		for (PacmanKeyListener listener:
+	         ui.getListeners(PacmanKeyListener.class)) {
+			System.out.print(listener);
+			listener.keyPressed(this.event);
+		}
 	}
 }
