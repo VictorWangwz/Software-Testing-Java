f924a27001c5ba9491e487bbba21ea8420d6f795
VictorWangwz <wangzhen5161@outlook.com>
2019-02-13 18:26:55 -0800

    add edge cases testing

--- jpacman-framework/src/test/java/org/jpacman/test/framework/model/BoardTest.java
+++ jpacman-framework/src/test/java/org/jpacman/test/framework/model/BoardTest.java
@@ -25,6 +25,22 @@ public class BoardTest {
 	}
 	
 	/**
+	 * Test the constructor with negative width and height
+	 */
+	@Test
+	public void testConstructor(){
+		try{
+			new Board(-1,10);
+		}catch(AssertionError e){
+		}
+		try{
+			new Board(10,-1);
+		}catch(AssertionError e){
+		}
+	}
+	
+	
+	/**
 	 * Test the getHeight() method
 	 */
 	@Test
@@ -42,7 +58,7 @@ public class BoardTest {
 	}
 	
 	/**
-	 * Test the put() method
+	 * Test the put() method, put sprite on tile without any assertions
 	 */
 	@Test
 	public void testSpritePutOnTile(){
@@ -50,11 +66,42 @@ public class BoardTest {
 		this.board.put(sprite, 1, 1);
 		assertEquals(this.board.tileAt(1, 1).topSprite(),sprite);
 	}
+	
+	/**
+	 * Test the put() method, put sprite on tile that is without board
+	 */
 	@Test
 	public void testSpritePutWithoutBoardOnTile(){
 		Sprite sprite = new Sprite(){};
 		try{
 			this.board.put(sprite, 11, 1);
+		}catch(AssertionError e){
+		}
+		
+	}
+	
+	/**
+	 * Test the put() method, put sprite null on tile
+	 */
+	@Test
+	public void testSpritePutNullOnTile(){
+		Sprite sprite = null;
+		try{
+			this.board.put(sprite, 1, 1);
+		}catch(AssertionError e){
+		}
+		
+	}
+	
+	/**
+	 * Test the put() method, put sprite that is already on tile onto tile
+	 */
+	@Test
+	public void testSpriteTileNullOnTile(){
+		Sprite sprite = new Sprite(){};
+		this.board.put(sprite, 1, 1);
+		try{
+			this.board.put(sprite, 2, 1);
 		}catch(AssertionError e){
 		}
 		
@@ -70,6 +117,20 @@ public class BoardTest {
 		assertEquals(this.board.spriteAt(1, 1), sprite);
 	}
 	
+	/**
+	 * Test the spriteAt() method with withinboarders() false
+	 */
+	@Test
+	public void testSpriteAtOutBoarders(){
+		Sprite sprite = new Sprite(){};
+		this.board.put(sprite, 1, 1);
+		try{
+			assertEquals(this.board.spriteAt(11, 1), sprite);
+		}catch(AssertionError e){
+		}
+		
+	}
+	
 	
 	/**
 	 * Test the spriteTypeAt() with new sprite method
@@ -80,6 +141,21 @@ public class BoardTest {
 		this.board.put(sprite, 1, 1);
 		assertEquals(this.board.spriteTypeAt(1, 1), sprite.getSpriteType());
 	}
+	
+	/**
+	 * Test the spriteTypeAt() method with withinboarders() false
+	 */
+	@Test
+	public void testSpriteTypeAtOutBoarders(){
+		Sprite sprite = new Sprite(){};
+		this.board.put(sprite, 1, 1);
+		try{
+			assertEquals(this.board.spriteTypeAt(11, 1), sprite.getSpriteType());
+		}catch(AssertionError e){
+		}
+		
+	}
+	
 	@Test
 	public void testNullSpriteTypeAt(){
 		assertEquals(this.board.spriteTypeAt(1, 1), SpriteType.EMPTY);
@@ -93,6 +169,19 @@ public class BoardTest {
 		Tile tile = this.board.tileAt(1,1);
 		assertEquals(tile.getX(), 1);
 		assertEquals(tile.getY(), 1);
+	}
+	
+	/**
+	 * Test the tileAt() method with withinboarders() false
+	 */
+	@Test
+	public void testTileAtOutBoarder(){
+		
+		try{
+			Tile tile = this.board.tileAt(11,11);
+		}catch(AssertionError e){
+		}
+		
 	}
 	
 	/**
@@ -113,6 +202,31 @@ public class BoardTest {
 		assertEquals(tile, this.board.tileAt(2, 2));
 	}
 	
+	/**
+	 * Test the tileAtOffside() method with Start as null
+	 */
+	@Test 
+	public void testTileAtOffsideStartNull(){
+		try{
+			Tile start= null;
+			Tile tile = this.board.tileAtOffset(start, 1, 1);
+		}catch(AssertionError e){
+		}
+		
+	}
+	
+	/**
+	 * Test the tileAtOffside() method with offset to negative
+	 */
+	@Test 
+	public void testTileAtOffsideNegative(){
+		try{
+			Tile start= this.board.tileAt(1,1);
+			Tile tile = this.board.tileAtOffset(start, 1, 1);
+		}catch(AssertionError e){
+		}
+		
+	}
 	
 	/**
 	 * Test the tileAtDirection() method
@@ -124,6 +238,7 @@ public class BoardTest {
 		Tile expectEnd = this.board.tileAt(0, 1);
 		assertEquals(tileEnd, expectEnd);
 	}
+	
 	
 	
 }
