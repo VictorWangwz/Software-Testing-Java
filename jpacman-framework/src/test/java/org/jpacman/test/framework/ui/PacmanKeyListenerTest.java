3f277aa01a7f479c54df6ec339ca7932f04db0e3
g-ziyan <gggongziyan@gmail.com>
2019-02-13 20:10:29 -0800

    modify Listener

--- jpacman-framework/src/test/java/org/jpacman/test/framework/ui/PacmanKeyListenerTest.java
+++ jpacman-framework/src/test/java/org/jpacman/test/framework/ui/PacmanKeyListenerTest.java
@@ -12,11 +12,17 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class PacmanKeyListenerTest {
-	KeyEvent event;
+	KeyEvent up;
+	KeyEvent down;
+	KeyEvent left;
+	KeyEvent right;
 	MainUI ui = new MainUI();
 	@Before
 	public void setUp(){
-		this.event = new KeyEvent(this.ui, 0, 0, 0, KeyEvent.VK_UP, 'a');
+		up = new KeyEvent(ui, 0, 0, 0, KeyEvent.VK_UP, 'a');
+		down = new KeyEvent(ui, 0, 0, 0, KeyEvent.VK_DOWN, 'b');
+		left = new KeyEvent(ui, 0, 0, 0, KeyEvent.VK_LEFT, 'c');
+		right = new KeyEvent(ui, 0, 0, 0, KeyEvent.VK_RIGHT, 'd');
 		}
 	@Test
 	public void testUIActions() throws FactoryException, InterruptedException, AWTException {
@@ -25,5 +31,12 @@ public class PacmanKeyListenerTest {
 		ui.initialize();
 		ui.start();
 		robot.keyPress(KeyEvent.VK_K);
+		robot.keyPress(KeyEvent.VK_J);
+		robot.keyPress(KeyEvent.VK_H);
+		robot.keyPress(KeyEvent.VK_L);
+		robot.keyPress(KeyEvent.VK_Q);
+		robot.keyPress(KeyEvent.VK_S);
+		robot.keyPress(KeyEvent.VK_X);
+	
 	}
 }
